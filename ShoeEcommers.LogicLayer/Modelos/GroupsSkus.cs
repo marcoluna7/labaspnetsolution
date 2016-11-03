3a681a8b086234f69184e2c7d695ee4db16bc183
marco <markorex7@gmail.com>
2016-11-03 12:59:43 -0600

    Se integra el rendereo de productos

---
+++ ShoeEcommers.LogicLayer/Modelos/GroupsSkus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoeEcommers.LogicLayer.Modelos
+{
+    public class GroupsSkusFront
+    {
+        public int IdProduct { get; set; }
+        public string ValueGroup { get; set; }
+        
+
+    }
+}
--- ShoesEcommers.ShopWeb/Controllers/ProductsController.cs
+++ ShoesEcommers.ShopWeb/Controllers/ProductsController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShoeEcommers.LogicLayer.Entities;
+using ShoeEcommers.LogicLayer.Modelos;
 
 namespace ShoesEcommers.ShopWeb.Controllers
 {
     public class ProductsController : Controller
     {
-        //
-        // GET: /Products/
+        public readonly ecommersEntities1 _dc;
+
+        public ProductsController()
+        {
+            _dc = new ecommersEntities1();
+        }
 
         public ActionResult Index()
         {
@@ -18,7 +24,15 @@ namespace ShoesEcommers.ShopWeb.Controllers
 
         public ActionResult Campaign(int id)
         {
-            return View();
+            var query = from p in _dc.Products
+                join s in _dc.Skus on p.Id equals s.IdProduct
+                join a in _dc.AttributeSkus on s.Id equals a.IdSku
+                        where p.Campaigns.Any(c => c.Id == id)
+                        group s by new GroupsSkusFront { IdProduct = p.Id, ValueGroup= a.ValueLabel } into groupsData
+                select groupsData;
+
+            List<IGrouping<GroupsSkusFront, Skus>> result = query.ToList();
+            return View(result);
         }
 
         public ActionResult Details(int id)
@@ -26,5 +40,12 @@ namespace ShoesEcommers.ShopWeb.Controllers
             return View();
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            _dc.Dispose();
+            base.Dispose(disposing);
+        }
     }
+
+    
 }
